1eeb64862e6efcb19f205fd02dccda4b5f6c6628
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2021-04-27 22:28:08 +0300

    burger menu add

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import calc from './modules/calc';
 import filters from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
+import burger from './modules/burger';
 
 window.addEventListener("DOMContentLoaded", () => {
     'use strict';
@@ -24,4 +25,5 @@ window.addEventListener("DOMContentLoaded", () => {
     filters();
     pictureSize('.sizes-block');
     accordion('.accordion-heading');
+    burger('.burger-menu', '.burger');
 });
